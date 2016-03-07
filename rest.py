f4920645ae9f90441568fb23c7dcec6f35c3e461
siddiq <siddiq235@gmail.com>
2016-03-06 23:18:17 -0800

    added cassandra

--- rest.py
+++ rest.py
@@ -5,22 +5,31 @@ from cassandra.cluster import Cluster
 
 
 cluster=Cluster()
-#json_file=open('task.json').read()
-#js = json.loads(json_file)
 session = cluster.connect('list')
 
 urls = (
-    '/task','get_task',
+    '/all','get_all_user',
     '/task/(.*)','get_task'
-
-
 )
 
 app = web.application(urls, globals())
+
+class get_all_user:
+    def GET(self):
+     query="select * from tasks"   
+     queryResult=session.execute(query) 
+     result="id name tittle priority creationTime endTime status postponeCount\n"
+     for r in queryResult:
+        result+=str(r.id)+" "+str(r.name)+" "+str(r.title)+" "+str(r.creationtime)+" "+str(r.endtime)+" "+str(r.status)+" "+str(r.postponecount)+"\n"
+     print result
+     return result
+
+
 class get_task:
     def GET(self,user):
         if len(user)==0:
             return "Usage:http://localhost:8080/task/arg1:name of user"
+       
         print "Username:",user
         query="""select * from tasks where name='"""+user+"""'"""   
         queryResult=session.execute(query)
@@ -32,9 +41,7 @@ class get_task:
        
     def POST(self):
         data = web.data()
-        #js['user'].append(str(data))
-        #json_file.write(js)
         print "New POST:",data
-        print "json\n",js
+        session.execute("""insert into tasks JSON '"""+str(data)+"""'""")
 if __name__ == "__main__":
     app.run()
